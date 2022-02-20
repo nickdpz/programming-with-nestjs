b1596fbe1d77c2c5fbd154841837575ff2550c28
nickdpz <ndpastranzamora@gmail.com>
2022-02-20 15:22:38 -0500

    :pencil: Response code http

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,20 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { CategoriesController } from './controllers/categories/categories.controller';
 import { ProductsController } from './controllers/products/products.controller';
+import { OrdersController } from './controllers/orders/orders.controller';
+import { UsersController } from './controllers/users/users.controller';
+import { CustomersController } from './controllers/customers/customers.controller';
 
 @Module({
   imports: [ConfigModule.forRoot({ envFilePath: '.env' })],
-  controllers: [AppController, CategoriesController, ProductsController],
+  controllers: [
+    AppController,
+    CategoriesController,
+    ProductsController,
+    OrdersController,
+    UsersController,
+    CustomersController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get, Query, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Body,
+  Post,
+  Delete,
+  Put,
+  HttpStatus,
+  HttpCode,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
   @Get()
-  getProducts(
+  list(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
@@ -17,7 +30,30 @@ export class ProductsController {
   }
 
   @Get(':productId')
-  getProduct(@Param('productId') productId: string) {
-    return `product ${productId}`;
+  @HttpCode(HttpStatus.ACCEPTED) // 👈 Using decorator
+  getOne(@Res() response: Response, @Param('productId') productId: string) {
+    response.status(200).send({ productId });
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    console.log(payload);
+    return {
+      message: 'accion de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return id;
   }
 }
